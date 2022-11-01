280734956eff97fc57a9f064e6d98aab55f5267c
Gaston-Tisera99 <gastontisera94@gmail>
2022-11-01 12:32:56 -0300

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-          Seguimiento Pacientes {""} 
+          Seguimiento Pacientes {""} Con GitHub {''} 
           <span className="text-indigo-600">Veterinaria</span>
         </h1>
        
